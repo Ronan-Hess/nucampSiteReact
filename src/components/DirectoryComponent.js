05c4c53e0ce2c4269450540665897170d7f62b60
Ronan Hess <gaberonanhess@gmail.com>
2020-04-25 11:25:54 -0500

    React Workshop 2. Created CampsiteInfoComponent.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 
 class Directory extends Component {
@@ -14,41 +15,24 @@ class Directory extends Component {
         this.setState({selectedCampsite: campsite});
     }
 
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
 
     render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
 
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                    {this.renderSelectedCampsite(this.state.selectedCampsite)}
+            <div>
+                {this.props.campsites.map(campsite => {
+                return (
+                    <div key={campsite.id} className="col-md-5 m-1">
+                        <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                            <CardImgOverlay>
+                                <CardTitle>{campsite.name}</CardTitle>
+                            </CardImgOverlay>
+                        </Card>
+                    </div>
+                );
+                })}
+                <CampsiteInfo campsite={this.state.selectedCampsite}/>
             </div>
         );
     }
