d81f4c144070b0eb70cb3d744f58c63e72705bf3
Ronan Hess <gaberonanhess@gmail.com>
2020-04-28 21:30:52 -0500

    Presentational and Container Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
+import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+
 
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
 
     render() {
-
-        return (
-            <div>
-                <div className="row">
-                    {this.props.campsites.map(campsite => {
-                    return (
-                        <div key={campsite.id} className="col-md-5 m-1">
-                            <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                                <CardImgOverlay>
-                                    <CardTitle>{campsite.name}</CardTitle>
-                                </CardImgOverlay>
-                            </Card>
-                        </div>
-                    );
-                    })}
+        const directory = this.props.campsites.map(campsite => {
+            return (
+          
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
-                    <CampsiteInfo campsite={this.state.selectedCampsite}/>
+             );
+        });
+        
+        return(
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
             </div>
         );
     }
